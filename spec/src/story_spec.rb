54f8cdb55ac64c8f95e276baf78650c0fbd8b5ed
Greg Lindsey <greg.lindsey@yello.co>
2020-02-10 12:13:42 -0600

    further analysis

--- spec/src/story_spec.rb
+++ spec/src/story_spec.rb
@@ -7,6 +7,13 @@ describe Story do
     let(:starting_vars) { {} }
     let(:results) { subject.traverse('intro', starting_vars) }
 
+    before do
+      allow(subject).to receive(:warn_cycle)
+      allow(subject).to receive(:warn_deadend)
+      allow(subject).to receive(:warn_decrement)
+      allow(subject).to receive(:warn_ending)
+    end
+
     context 'with a single-node YML file' do
       subject { described_class.new('spec/src/data/single_node.yml') }
 
@@ -115,24 +122,32 @@ describe Story do
           expect(results.length).to eq(1)
         end
 
-        it 'returns the crumb path "intro -> ending"' do
-          expect(results[0]['crumbs']).to eq(['intro', 'ending'])
+        it 'returns the crumb path "intro"' do
+          expect(results[0]['crumbs']).to eq(['intro'])
+        end
+
+        it 'warns the user of a dead-end' do
+          allow(subject).to receive(:warn_deadend)
+
+          results
+
+          expect(subject).to have_received(:warn_deadend)
         end
       end
 
       context 'with flag set' do
         let(:starting_vars) { { 'can_reach_flag_node' => true } }
 
-        it 'returns two traversal paths' do
-          expect(results.length).to eq(2)
+        it 'returns one traversal path' do
+          expect(results.length).to eq(1)
         end
       end
 
       context 'with value set' do
         let(:starting_vars) { { 'must_exceed_three' => 5 } }
 
-        it 'returns two traversal paths' do
-          expect(results.length).to eq(2)
+        it 'returns one traversal path' do
+          expect(results.length).to eq(1)
         end
       end
 
@@ -144,8 +159,8 @@ describe Story do
           }
         end
 
-        it 'returns three traversal paths' do
-          expect(results.length).to eq(3)
+        it 'returns two traversal paths' do
+          expect(results.length).to eq(2)
         end
       end
     end
--- src/story.rb
+++ src/story.rb
@@ -24,21 +24,22 @@ class Story
 
     crumbs.push(id)
 
-    result_data = [{
-      'crumbs' => crumbs,
-      'vars' => vars
-    }]
-
-    # First check if the node was already visited.
-    if crumbs_orig.include?(id)
-      warn_cycle(id)
-      return result_data
-    end
-
     # Duplicate the node/vars at this level.
     node = data[id]
 
     raise TypeError, 'expected node to be Hash' unless node.is_a?(Hash)
+
+    result_data = {
+      'crumbs' => crumbs,
+      'vars' => vars
+    }
+
+    # First check if the node was already visited.
+    if crumbs_orig.include?(id)
+      warn_cycle(id)
+      return [result_data]
+    end
+
 
     if node['flags'].is_a?(Hash)
       node['flags'].each do |(flagname, value)|
@@ -72,6 +73,10 @@ class Story
       end
     end
 
+    link_ids = Set.new((node['links'] || {}).keys)
+    result_data['links'] = link_ids
+    result_data['locked_links'] = Set.new
+
     link_results = []
     if node['links'].is_a?(Hash)
       node['links'].each do |(link, conditions)|
@@ -84,8 +89,6 @@ class Story
               can_visit &= ((vars[flagname] || false) == value)
             end
           end
-
-          next unless can_visit
 
           if value_conditions.is_a?(Hash)
             value_conditions.each do |varname, rules|
@@ -104,8 +107,6 @@ class Story
               else
                 raise TypeError, "invalid comparison operator '#{op}' provided"
               end
-
-              break unless can_visit
             end
           end
         end
@@ -114,12 +115,43 @@ class Story
           link_results.concat(
             traverse(link, vars, crumbs)
           )
+        else
+          result_data['links'].delete(link)
+          result_data['locked_links'].add(link)
         end
       end
     end
 
+    if result_data['links'].empty?
+      if !result_data['locked_links'].empty?
+        warn_deadend(id, crumbs, vars)
+      elsif vars['ending'] != true
+        warn_ending(id, crumbs, vars)
+      end
+    end
+
     # End of links; return accumulated link results
-    link_results.empty? ? result_data : link_results
+    link_results.empty? ? [result_data] : link_results
+  end
+
+  def analyze_coverage(results)
+    # Each record in `results` contains the breadcrumb trail used to get there,
+    # and the final variables arrived at.
+
+    # Empty value Hash of all node IDs in the story.
+    story_nodes = data.each_with_object({}) { |node, obj| obj[node] = {} }
+
+    # Iterate through the branches...
+    results.each do |path|
+      # Iterate through the nodes in the trail.
+      path['crumbs'].each do |crumb|
+        # Create or add to visit count for this node.
+        story_nodes[crumb]['visit_count'] ||= 0
+        story_nodes[crumb]['visit_count'] += 1
+      end
+    end
+
+    { nodes: story_nodes }
   end
 
   def warn_cycle(id)
@@ -127,7 +159,20 @@ class Story
          "bailing out of possible cycle"
   end
 
+  def warn_deadend(id, crumbs, vars)
+    warn "Dead end encountered at node ID '#{id}' -- all links locked"
+    warn "Breadcrumb trail: #{crumbs}"
+    warn "Variables: #{vars}"
+  end
+
   def warn_decrement(id, varname)
     warn "Node ID '#{id}' -- decrementing '#{varname}' drops it below zero"
+  end
+
+  def warn_ending(id, crumbs, vars)
+    warn "Dead end encountered at node ID '#{id}' -- " \
+         "no links present and ending flag not set"
+    warn "Breadcrumb trail: #{crumbs}"
+    warn "Variables: #{vars}"
   end
 end
